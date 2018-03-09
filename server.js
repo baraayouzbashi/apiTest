c0a81049bd02b7cc87aff2a2da2cff71e3c49e88
Baraa Youzbashi <bara2@hotmail.co.uk>
2018-03-09 21:13:16 +0200

    init

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var express = require
+var express = require('express');
+var app = express();
+var PORT = process.env.port || 3000;
+
+
+app.get('/',function (req,res) {
+	res.send('API Root');
+});
+
+app.listen(PORT,function(){
+	console.log('listening on port '+ PORT);
+});
